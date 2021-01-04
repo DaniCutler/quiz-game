046291156f14a73e4664a6f586f1d6985d35e4e3
DaniCutler <72281065+DaniCutler@users.noreply.github.com>
2021-01-03 19:27:01 -0700

    made changes with tutor to get app working
    made changes to local storage so that the score is able to save

--- end.js
+++ end.js
@@ -1,15 +1,18 @@
 const username = document.querySelector('#username')
 const saveScorBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = document.localStorage.getItem('mostRecentScore')
-
+// const mostRecentScore = document.localStorage.getItem('mostRecentScore')
+const currentScore = document.getElementById('score').innerText
+console.log(currentScore)
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 const MAX_HIGH_SCORES = 5
-finalScore.innerText = mostRecentScore
+const mostRecentScore = document.getElementById('finalScore').innerText
+console.log(mostRecentScore)
 
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.nodeValue
-})
+
+// username.addEventListener('keyup', () => {
+//     saveScoreBtn.disabled = !username.nodeValue
+// })
 
 saveHighScore = e => {
     e.preventDefault()
@@ -18,13 +21,14 @@ saveHighScore = e => {
         score: mostRecentScore,
         name: username.value
     }
-
+    console.log(score.score)
     highScores.push(score)
     highScores.sort((a,b) => {
         return b.score - a.score
     })
 
     highScores.splice (5) 
+    console.log(highScores)
     localStorage.setItem('highScores', JSON.stringify (highScores))
     window.location.assign('/')
 }
